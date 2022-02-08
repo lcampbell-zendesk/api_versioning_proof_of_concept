bafda6b43dbedafec344593d225fad08f7a80e16
Erik Trapin (ecoologic) <erik.trapin@zendesk.com>
2022-02-09 08:09:45 +1000

    firstname -> first_name

--- server.rb
+++ server.rb
@@ -1,15 +1,18 @@
 require 'sinatra'
 
-# curl "localhost:4567/?firstname=erik"
+# v1 curl "localhost:4567/?firstname=erik"
+# v2 curl "localhost:4567/?first_name=erik"
 get '/' do
   updated_request = versioned_request(request)
 
-  response_body = updated_request.params["firstname"].to_s + " is awesome"
+  response_body = updated_request.params["first_name"].to_s + " is awesome"
 
   versioned_response_body(response_body)
 end
 
 def versioned_request(request)
+  request.params.merge("first_name" => request.params["firstname"])
+  request.params.delete("firstname")
   request
 end
 
