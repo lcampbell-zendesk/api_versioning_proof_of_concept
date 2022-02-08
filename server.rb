fef85425b4481c8828fd0040b61dec4d63922ab4
Erik Trapin (ecoologic) <erik.trapin@zendesk.com>
2022-02-09 08:35:21 +1000

    module Changes

--- server.rb
+++ server.rb
@@ -2,20 +2,54 @@ require 'sinatra'
 
 # v1 curl "localhost:4567/?firstname=erik&what=modest"
 # v2 curl "localhost:4567/?first_name=erik&what=modest" (firstname -> first_name)
+# v3 curl "localhost:4567/?first_name=erik&what=modest" (what -> is)
 get '/' do
   updated_request = versioned_request(request)
 
-  response_body = "#{updated_request.params["first_name"]} is #{updated_request.params["what"]}"
+  response_body = "#{updated_request.params['first_name']} is #{updated_request.params['what']}"
 
   versioned_response_body(response_body)
 end
 
 def versioned_request(request)
-  request.params.merge("first_name" => request.params["firstname"])
-  request.params.delete("firstname")
-  request
+  Changes::BY_ENDPOINT['GET_/'].reduce(request) do |_, change_class|
+    change_class.new(request).call
+  end
 end
 
 def versioned_response_body(response_body)
   response_body
+end
+
+module Changes
+  # Implement me in subclasses
+  class AbstractRequest
+    def initialize(request)
+      @request = request
+    end
+
+    def call
+      raise NotImplementedError
+    end
+
+    private
+
+    attr_reader :request
+  end
+
+  # RenameRequestParamFromFirstnameToFirstName
+  class RenameRequestParamFromFirstnameToFirstName < AbstractRequest
+    def call
+      request.params.merge('first_name' => request.params['firstname'])
+      request.params.delete('firstname')
+      request
+    end
+  end
+
+  BY_ENDPOINT = {
+    'GET_/' => [
+      RenameRequestParamFromFirstnameToFirstName
+      # RenameRequestParamFromWhatToIs
+    ]
+  }
 end
