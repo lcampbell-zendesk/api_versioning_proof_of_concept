e1804c02fd269cce875ad8eb9ef0a9d1534e608a
Erik Trapin (ecoologic) <erik.trapin@zendesk.com>
2022-02-09 12:24:38 +1000

    Changes::RenameRequestParamFromFirstNameToFullName 2023 first_name -> full_name

--- server.rb
+++ server.rb
@@ -3,11 +3,12 @@ require 'sinatra'
 # curl -H "X-ZendeskAPI-Version: 2020" "localhost:4567/?firstname=Zendesk&what=zen"
 # curl -H "X-ZendeskAPI-Version: 2021" "localhost:4567/?first_name=Programming&what=cool" # (firstname -> first_name)
 # curl -H "X-ZendeskAPI-Version: 2022" "localhost:4567/?first_name=Teamwork&is=awesome" # (what -> is)
+# curl -H "X-ZendeskAPI-Version: 2023" "localhost:4567/?full_name=Teamwork&is=awesome" # (first_name -> full_name)
 get '/' do
   updated_request = versioned_request(request)
 
   # Whatever Business Logic, uses latest version
-  response_body = "#{updated_request.params['first_name']} is #{updated_request.params['is']}"
+  response_body = "#{updated_request.params['full_name']} is #{updated_request.params['is']}"
 
   versioned_response_body(response_body)
 end
@@ -74,12 +75,19 @@ module Changes
       rename_request_param(from: 'what', to: 'is')
     end
   end
+
+  class RenameRequestParamFromFirstNameToFullName < AbstractRequest
+    def apply
+      rename_request_param(from: 'first_name', to: 'full_name')
+    end
+  end
 end
 
 class VersionChanges
   VERSIONS = {
     '2020' => [],
     '2021' => [Changes::RenameRequestParamFromFirstnameToFirstName],
-    '2022' => [Changes::RenameRequestParamFromWhatToIs]
+    '2022' => [Changes::RenameRequestParamFromWhatToIs],
+    '2023' => [Changes::RenameRequestParamFromFirstNameToFullName]
   }
 end
