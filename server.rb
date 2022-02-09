966f174b05f3236ed60c01203be530066eaf022c
Erik Trapin (ecoologic) <erik.trapin@zendesk.com>
2022-02-09 11:55:40 +1000

    VersionChanges::VERSIONS values as array

--- server.rb
+++ server.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 
-# curl -H "X-ZendeskAPI-Version: 2020" "localhost:4567/?firstname=erik&what=modest"
-# curl -H "X-ZendeskAPI-Version: 2021" "localhost:4567/?first_name=erik&what=modest" # (firstname -> first_name)
-# curl -H "X-ZendeskAPI-Version: 2022" "localhost:4567/?first_name=erik&is=awesome" # (what -> is)
+# curl -H "X-ZendeskAPI-Version: 2020" "localhost:4567/?firstname=Zendesk&what=zen"
+# curl -H "X-ZendeskAPI-Version: 2021" "localhost:4567/?first_name=Programming&what=cool" # (firstname -> first_name)
+# curl -H "X-ZendeskAPI-Version: 2022" "localhost:4567/?first_name=Teamwork&is=awesome" # (what -> is)
 get '/' do
   updated_request = versioned_request(request)
 
@@ -15,13 +15,13 @@ end
 # VERSIONING LOGIC
 
 def versioned_request(request)
-  version_changes(request.env).reduce(request) do |_, change_class|
+  version_changes(request.env).flatten.reduce(request) do |_, change_class|
     change_class.new(request).call
   end
 end
 
 def versioned_response_body(response_body)
-  response_body
+  response_body # TODO:
 end
 
 def relevant_versions(headers)
@@ -45,7 +45,6 @@ module Changes
 
     def call
       apply
-      puts "call #{request.class}"
       request
     end
 
@@ -55,24 +54,21 @@ module Changes
       raise NotImplementedError
     end
 
-    # TODO: use composition instead of helper methods
+    # TODO: use composition instead of shared helper methods
     def rename_request_param(from:, to:)
       request.params.merge!(to => request.params[from])
       request.params.delete(from)
-      puts "rename_request_param #{request.params.inspect}"
     end
 
     attr_reader :request
   end
 
-  # RenameRequestParamFromFirstnameToFirstName
   class RenameRequestParamFromFirstnameToFirstName < AbstractRequest
     def apply
       rename_request_param(from: 'firstname', to: 'first_name')
     end
   end
 
-  # RenameRequestParamFromWhatToIs
   class RenameRequestParamFromWhatToIs < AbstractRequest
     def apply
       rename_request_param(from: 'what', to: 'is')
@@ -82,9 +78,8 @@ end
 
 class VersionChanges
   VERSIONS = {
-    # TODO: value should be array
-    '2020' => Changes::AbstractRequest,
-    '2021' => Changes::RenameRequestParamFromFirstnameToFirstName,
-    '2022' => Changes::RenameRequestParamFromWhatToIs
+    '2020' => [],
+    '2021' => [Changes::RenameRequestParamFromFirstnameToFirstName],
+    '2022' => [Changes::RenameRequestParamFromWhatToIs]
   }
 end
